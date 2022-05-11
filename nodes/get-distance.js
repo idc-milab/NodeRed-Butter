d42632938ae2a3cef77707558936495c374c5582
ehudb9 <70571624+ehudb9@users.noreply.github.com>
2022-05-11 13:36:10 +0300

    python scripts connector

--- nodes/get-distance.js
+++ nodes/get-distance.js
@@ -5,14 +5,14 @@
         * output: operation success/failure response.
 */
 module.exports = function(RED) {
-	function SetMotorRegisterNode(config) {
+	function GetDistNode(config) {
 		RED.nodes.createNode(this, config);
 		this.config = config;
 
 		const DebugLogger = require('../logger/debug_logger');
-		const butterClientProvider = require('../butter-client/butter-client-provider');
+		//const butterClientProvider = require('../butter-client/butter-client-provider');
 
-		this.butterHttpClient = butterClientProvider.GetClient(this.config.robotIp);
+		//this.butterHttpClient = butterClientProvider.GetClient(this.config.robotIp);
 		this.debugLogger = new DebugLogger(this, this.config.debugMode);
 
 		this.on('input', async function(msg) {
@@ -40,17 +40,59 @@ module.exports = function(RED) {
 			);
 
 			try {
-				butter_response = await this.butterHttpClient.setMotorRegister(motorName, registerName, value);
+				//butter_response = await this.butterHttpClient.setMotorRegister(motorName, registerName, value);
+				alert("finally");
 
-				this.debugLogger.logIfDebugMode(`butter response is ${JSON.stringify(butter_response.data)}`);
+
+				var express = require("express");
+				const axios = require("axios");
+				var app = express();
+				var response = "";
+
+				app.listen(3000, function() {
+				console.log("server running on port 3000");
+				});
+
+				app.get("/", get_distance);
+
+				function get_distance(req, res) {
+				// Use child_process.spawn method from
+				// child_process module and assign it
+				// to variable spawn
+				var spawn = require("child_process").spawn;
+
+				// E.g : http://localhost:3000/name?firstname=Mike&lastname=Will
+				// so, first name = Mike and last name = Will
+				var process = spawn("python", [
+					"./sensors.py",
+					req.query.firstname,
+					req.query.lastname
+				]);
+
+				process.stdout.on("data", function(data) {
+					response = data.toString();
+				});
+				}
+
+				axios
+				.get("http://localhost:3000/?firstname=Omer&lastname=Noam")
+				.then(response => {
+					console.log(response.data);
+					// console.log(response.data.explanation);
+				})
+				.catch(error => {
+					console.log(error);
+				});
+
+				//this.debugLogger.logIfDebugMode(`butter response is ${JSON.stringify(butter_response.data)}`);
 				// send operation result.
-				this.send({ payload: butter_response.data });
+				this.send({ payload: response });
 			} catch (error) {
-				this.debugLogger.logIfDebugMode(`failed to set register ${registerName}\n${error}`);
+				this.debugLogger.logIfDebugMode(`failed to get distance`);
 			}
 		});
 	}
 
 	// register node type.
-	RED.nodes.registerType('set-motor-register', SetMotorRegisterNode);
+	RED.nodes.registerType('get-distance', GetDistNode);
 };
---
+++ nodes/sensors.py
@@ -0,0 +1,9 @@
+import sys
+
+print("hello!\n")
+print("Output from Python")
+print("First name: " + sys.argv[1])
+print("Last name: " + sys.argv[2])
+print("your dist is 10Km")
+
+sys.stdout.flush()
