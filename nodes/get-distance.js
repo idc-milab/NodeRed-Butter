e2269e012e055386425cd74ebba1b12181a2c2d1
ehudb9 <70571624+ehudb9@users.noreply.github.com>
2022-05-18 14:02:30 +0300

    Update get-distance.js

--- nodes/get-distance.js
+++ nodes/get-distance.js
@@ -10,6 +10,36 @@ module.exports = function(RED) {
 		//this.butterHttpClient = butterClientProvider.GetClient(this.config.robotIp);
 		this.debugLogger = new DebugLogger(this, this.config.debugMode);
 
+		var express = require("express");
+		const axios = require("axios");
+		var app = express();
+		var response = "";
+
+		app.listen(3003, function() {
+		console.log("server running on port 3003");
+		});
+
+		app.get("/name", get_distance);
+
+		function get_distance(req, res) {
+			console.log("kkkkkllkk");
+			var spawn = require("child_process").spawn;
+
+			// E.g : http://localhost:3000/name?firstname=Mike&lastname=Will
+			// so, first name = Mike and last name = Will
+			var process = spawn("python", [
+				"/Users/ehudb/Documents/GitHub/NodeRed-Butter/nodes/sensors.py",
+				req.query.firstname,
+				req.query.lastname
+			]);
+
+			process.stdout.on("data", function(data) {
+				// console.log(data.toString());
+				res.send(data.toString());
+			});
+		}
+		
+
 		this.on('input', async function(msg) {
 			// let robotIp = this.config.robotIp;
 			// let motorName = this.config.motorName;
@@ -36,54 +66,23 @@ module.exports = function(RED) {
 
 			try {
 				//butter_response = await this.butterHttpClient.setMotorRegister(motorName, registerName, value);
-
-
-				var express = require("express");
-				const axios = require("axios");
-				var app = express();
-				var response = "";
-
-				app.listen(3003, function() {
-				console.log("server running on port 3003");
-				axios.get("http://localhost:3003/n?firstname=Omer&lastname=Noam").then(response => {
-					console.log("omer&noam");
-					console.log(data.toString());
+				console.log("ILOVEBeny");
+				axios.get("http://127.0.0.1:3003/name?firstname=Omer&lastname=Noam").then(response => {
+					console.log(response.data);
+					console.log(JSON.stringify(response));
 	
-					this.send({ payload: response });		
+					this.send({ payload: response });
+					this.debugLogger.logIfDebugMode(`butter response is ${response.data.toString()}`);
+
 				})
 				.catch(error => {
 					console.log(error);
 				});
-				
-				});
 
-				app.get("/n", get_distance);
-
-				function get_distance(req, res) {
-					// Use child_process.spawn method from
-					// child_process module and assign it
-					// to variable spawn
-					var spawn = require("child_process").spawn;
-
-					// E.g : http://localhost:3000/name?firstname=Mike&lastname=Will
-					// so, first name = Mike and last name = Will
-					var process = spawn("python", [
-						"./sensors.py",
-						req.query.firstname,
-						req.query.lastname
-					]);
-
-					process.stdout.on("data", function(data) {
-						res.send(data.toString());
-						this.debugLogger.logIfDebugMode(`butter response is ${data.toString()}`);
-						
-						
-					});
-				}
-				
-				
 				
 			} catch (error) {
+				console.log(error);
+
 				this.debugLogger.logIfDebugMode(error);
 
 				this.debugLogger.logIfDebugMode(`failed to get distance`);
